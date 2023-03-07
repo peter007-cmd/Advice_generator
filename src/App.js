57e2d9718487a4945476ca248b73c758c66667a6
Peter <petermj94@gmail.com>
2023-03-07 12:15:25 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[Post,setPost] = useState({})
+  const fetchData = ()=>{
+    axios.get('https://api.adviceslip.com/advice').then(res=>{
+      console.log(res)
+      setPost(res)
+    })
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
+  // fetchData()
+return(
+  <div className="App">
+    <h2>ADVICE {Post.data.slip.id}</h2>
+    <p>{Post.data.slip.advice}</p>
+    <button onClick={fetchData}>Get Data</button>
+    
     </div>
-  );
+  
+)
+
+    
+
 }
 
 export default App;
